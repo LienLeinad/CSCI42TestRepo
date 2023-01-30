1a71f907d3d6a9a966237539841754f21ab5b368
Neil Bautista <33220418+LienLeinad@users.noreply.github.com>
2023-01-30 21:17:14 +0800

    [RevertBack]] Everything (#6)

--- GitTutorial.py
+++ GitTutorial.py
@@ -13,11 +13,33 @@ class Adder:
             returns sum of x and y args
         """
         return x + y 
+    def add_2(self, x):
+        """
+            returns sum of 2 and x arg
+        """
+        return x + 2
+class Multiplyer:
+    def multiply(self, x, y):
+        """
+            returns product of x and y args
+        """
+        return x * y
 
+class Subtractor:
+    def subtract(self, x, y):
+        """
+            returns difference of x and y args
+        """
+        return x - y
     
 def main():
     adder_obj = Adder()
+    multiplyer_obj = Multiplyer()
+
     print(adder_obj.sum(1,1))
+    print(adder_obj.add_2(2))
+
+    print(multiplyer_obj.multiply(2,2))
 
 if __name__ == "__main__":
     main()
