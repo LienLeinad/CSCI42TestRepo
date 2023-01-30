cb5bfe35035f008f332b6b1db86df36625c590a7
LienLeinad <neil@bukas.ph>
2023-01-30 20:55:48 +0800

    finish add_2 method

--- GitTutorial.py
+++ GitTutorial.py
@@ -13,7 +13,11 @@ class Adder:
             returns sum of x and y args
         """
         return x + y 
-
+    def add_2(self, x):
+        """
+            returns sum of 2 and x arg
+        """
+        return x + 2
 class Multiplyer:
     def multiply(self, x, y):
         """
@@ -26,6 +30,7 @@ def main():
     multiplyer_obj = Multiplyer()
 
     print(adder_obj.sum(1,1))
+    print(adder_obj.add_2(2))
 
     print(multiplyer_obj.multiply(2,2))
 
