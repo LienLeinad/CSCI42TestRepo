00e9d771267cc3234e3598e84aa8a64667c940ca
LienLeinad <neil@bukas.ph>
2023-01-30 20:58:41 +0800

    add multiplyer object and multiply method

--- GitTutorial.py
+++ GitTutorial.py
@@ -14,10 +14,20 @@ class Adder:
         """
         return x + y 
 
+class Multiplyer:
+    def multiply(self, x, y):
+        """
+            returns product of x and y args
+        """
+        return x*y
     
 def main():
     adder_obj = Adder()
+    multiplyer_obj = Multiplyer()
+
     print(adder_obj.sum(1,1))
+
+    print(multiplyer_obj.multiply(2,2))
 
 if __name__ == "__main__":
     main()
