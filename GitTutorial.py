cd7e5243f6a3e1653ba2ade0a87f91975ef55ecc
LienLeinad <neil@bukas.ph>
2023-01-31 08:22:03 +0800

    added add_2 method to adder class

--- GitTutorial.py
+++ GitTutorial.py
@@ -13,10 +13,16 @@ class Adder:
         """
         return x + y
 
+    @classmethod
+    def add_2(cls, x):
+        """
+            Returns sum of 2 and x
+        """
+        return 2 + x
 
 def main():
     print(Adder.sum(1, 1))
-
+    print(Adder.add_2(1))
 
 if __name__ == "__main__":
     main()
